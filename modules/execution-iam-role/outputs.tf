433940875370ac6b52be9cfb4c2f09c933545b6c
Michael Ullrich <michael.ullrich@nuvibit.com>
2024-04-05 23:38:44 +0200

    fix

--- modules/execution-iam-role/outputs.tf
+++ modules/execution-iam-role/outputs.tf
@@ -6,7 +6,5 @@ output "lambda_execution_iam_role" {
     "name"                                                     = local.create_new_execution_iam_role ? aws_iam_role.execution_role[0].name : data.aws_iam_role.existing_execution_iam_role[0].name,
     "arn"                                                      = local.create_new_execution_iam_role ? aws_iam_role.execution_role[0].arn : data.aws_iam_role.existing_execution_iam_role[0].arn
     "aws_iam_policy_document"                                  = data.aws_iam_policy_document.new_lambda_permission_policies
-    "local.create_new_execution_iam_role"                      = local.create_new_execution_iam_role
-    "local.new_execution_iam_role.permission_policy_json_list" = local.new_execution_iam_role.permission_policy_json_list
   }
 }
