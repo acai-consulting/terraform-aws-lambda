3f58442608cfcb640fdc57314695e91821e1a2e8
Michael Ullrich <michael.ullrich@nuvibit.com>
2024-04-06 07:58:09 +0200

    fix tc3

--- test/use-case-3_test.go
+++ test/use-case-3_test.go
@@ -20,12 +20,21 @@ func TestLambdaUC3(t *testing.T) {
 	defer terraform.Destroy(t, terraformOptions)
 	terraform.InitAndApply(t, terraformOptions)
 
-	lambdaResultOutput := terraform.OutputMap(t, terraformOptions, "use_case_3_lambda_result")
-	t.Logf("Lambda Output: %s", lambdaResultOutput)
+	lambdaResultOutput1 := terraform.OutputMap(t, terraformOptions, "use_case_3_lambda1_result")
+	t.Logf("Lambda Output1: %s", lambdaResultOutput1)
 
 	// Extract the statusCode and assert it
-	statusCode := lambdaResultOutput["statusCode"]
+	statusCode1 := lambdaResultOutput1["statusCode"]
 	// Print the status code
-	t.Logf("Derived StatusCode: %s", statusCode)
-	assert.Equal(t, "200", statusCode, "Expected statusCode to be 200")
+	t.Logf("Derived StatusCode1: %s", statusCode1)
+	assert.Equal(t, "200", statusCode1, "Expected statusCode to be 200")
+
+	lambdaResultOutput2 := terraform.OutputMap(t, terraformOptions, "use_case_3_lambda2_result")
+	t.Logf("Lambda Output2: %s", lambdaResultOutput2)
+
+	// Extract the statusCode and assert it
+	statusCode2 := lambdaResultOutput2["statusCode"]
+	// Print the status code
+	t.Logf("Derived StatusCode: %s", statusCode2)
+	assert.Equal(t, "200", statusCode2, "Expected statusCode to be 200")
 }
