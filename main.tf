07e0b4dea0d8e995cb81bb3eda70cc5d1bbbfca2
Michael Ullrich <michael.ullrich@acai.gmbh>
2024-06-07 16:06:08 +0200

    fix

--- main.tf
+++ main.tf
@@ -14,6 +14,10 @@ terraform {
       source  = "hashicorp/archive"
       version = ">= 2.0.0"
     }
+    local = {
+      source  = "hashicorp/local"
+      version = "= 2.5.1"
+    }    
   }
 }
 
